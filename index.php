eaa75ba231d1bbee497edad89f9ab65b65e2e2b2
Delgesu2 <xvr.cello@gmail.com>
2017-03-24 02:45:22 +0100

    Un peu de Bootstrap ajouté

--- index.php
+++ index.php
@@ -5,9 +5,14 @@
  * Date: 23/03/17
  * Time: 15:42
  */
+
+
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -20,4 +25,7 @@
     <input type="submit" value="Send" />
 </form>
 </body>
+
 </html>
+
+
--- resultat.php
+++ resultat.php
@@ -6,6 +6,8 @@
  * Time: 15:52
  */
 
+
+
 if(isset($_GET['produit'])) {
 
     $url = 'https://world.openfoodfacts.org/cgi/search.pl?search_terms=' . $_GET["produit"] . '&search_simple=1action=process&json=1';
@@ -15,16 +17,30 @@ if(isset($_GET['produit'])) {
     $json = json_decode($result, true);
     //var_dump($json);   //uncom for check all product category
     foreach ($json['products'] as $row) {
-        //var_dump($row['']); // test pour récuperer des données
+        // var_dump($row['']); //
         $productName = $row['product_name'];
         $brand = $row['brands'];
-        $nut = $row['nutrition-score-fr'];
+
+        if (isset($row['nutrition_grades']))
+        {
+            $nut = $row['nutrition_grades'];
+        }
+        else if(isset($row['nutrition_grade_fr']))
+
+        {
+            $nut = $row['nutrition_grade_fr'];
+        }
+
+
+        $energy = $row ['nutriments']['energy']*0.239006;
         $image = $row['image_small_url'];
         $viewData = file_get_contents('reponse.html');
 
+            $nut=strtoupper($nut);
+
         echo str_replace(
             ['{productName}', '{brand}', '{image}', '{nut}'],
-            [$productName, $brand, $image, $nut],
+            [$productName, $brand, $image, $nut, $energy],
             $viewData);
     }
 }
